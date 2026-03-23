9139c17d6c552f22771c15b13dd998880c45b2e3
trung1609 <vmt162005@gmail.com>
2026-03-23 20:18:36 +0700

    Initial Spring Boot application setup with Gradle

---
+++ src/main/java/com/trung/IntershipManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.trung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntershipManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntershipManagementSystemApplication.class, args);
+	}
+
+}
